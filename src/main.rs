c8cdf754bef0fe212a8be4ab8acce41115693518
Joseph Mertens <privateer180@gmail.com>
2024-07-30 20:21:24 -0500

    feat: add clap cli and readout of some basic file metadata

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+use mp4ameta::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let file = mp4ameta::Tag::read_from_path(args.file).unwrap();
+
+    let artist = file.artist();
+    let year = file.year();
+
+    println!("Artist: {:?}", artist);
+    println!("Year: {:?}", year);
 }
+
+/// A simple program to read and write audio file tags
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = "None")]
+pub struct Args {
+
+    /// target audio file
+    #[arg(short, long)]
+    file: String,
+}
+
+
