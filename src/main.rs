d5dc3352b67aac389a4ee2e68893fdeb2d0b1991
Joseph Mertens <privateer180@gmail.com>
2024-07-30 20:51:55 -0500

    feat: build out basic read from mp3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use clap::Parser;
 use mp4ameta::*;
+use id3::*;
+use log::info;
 
 fn main() {
     let args = Args::parse();
-    let file = mp4ameta::Tag::read_from_path(args.file).unwrap();
+    
+    if args.file.contains(".mp3") {
+        info!("parsing mp3 file");
+        let file = id3::Tag::read_from_path(args.file).unwrap();
+        let artist = file.artist();
+        let year = file.year();
+        println!("Artist: {:?}", artist);
+        println!("Year: {:?}", year);
+    } else {
+        info!("parsing m4a file");
+        let file = mp4ameta::Tag::read_from_path(args.file).unwrap();
 
-    let artist = file.artist();
-    let year = file.year();
+        let artist = file.artist();
+        let year = file.year();
 
-    println!("Artist: {:?}", artist);
-    println!("Year: {:?}", year);
+        println!("Artist: {:?}", artist);
+        println!("Year: {:?}", year);
+     
+    }
 }
 
 /// A simple program to read and write audio file tags
