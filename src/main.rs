ce07c99e58bda74d25da9d342e9c841d1ef3d071
Joseph Mertens <privateer180@gmail.com>
2024-07-31 21:18:43 -0500

    feat: add basic logger impl; add file type enum and file extension parsing

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
 use clap::Parser;
-use mp4ameta::*;
 use id3::*;
-use log::info;
+use simple_logger::SimpleLogger;
 
 fn main() {
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .init()
+        .unwrap();
+
     let args = Args::parse();
-    
-    if args.file.contains(".mp3") {
-        info!("parsing mp3 file");
-        let file = id3::Tag::read_from_path(args.file).unwrap();
-        let artist = file.artist();
-        let year = file.year();
-        println!("Artist: {:?}", artist);
-        println!("Year: {:?}", year);
-    } else {
-        info!("parsing m4a file");
-        let file = mp4ameta::Tag::read_from_path(args.file).unwrap();
 
-        let artist = file.artist();
-        let year = file.year();
+    let extension = get_extension(&args.file);
 
-        println!("Artist: {:?}", artist);
-        println!("Year: {:?}", year);
-     
+    log::info!("extension: {:?}", extension);
+    match extension {
+        FileExtension::Mp3 => {
+            log::info!("parsing mp3 file");
+            let file = id3::Tag::read_from_path(args.file).unwrap();
+            let artist = file.artist();
+            let year = file.year();
+            println!("Artist: {:?}", artist);
+            println!("Year: {:?}", year);
+        }
+        FileExtension::M4a => {
+            log::info!("parsing m4a file");
+            let file = mp4ameta::Tag::read_from_path(args.file).unwrap();
+
+            let artist = file.artist();
+            let year = file.year();
+
+            println!("Artist: {:?}", artist);
+            println!("Year: {:?}", year);
+        }
     }
 }
 
@@ -30,10 +39,22 @@ fn main() {
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = "None")]
 pub struct Args {
-
     /// target audio file
     #[arg(short, long)]
     file: String,
 }
 
+pub fn get_extension(path: &str) -> FileExtension {
+    let file = path.to_string();
+    match file.split('.').last().unwrap().to_lowercase().as_str() {
+        "mp3" => FileExtension::Mp3,
+        "m4a" => FileExtension::M4a,
+        _default => panic!("No valid file extension found! Cannot determine file type"),
+    }
+}
 
+#[derive(Debug)]
+pub enum FileExtension {
+    Mp3,
+    M4a,
+}
